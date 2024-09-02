387b5cf9790b5fde2a091dd30c395ac8a18401d0
alumno <alumno@iMac-V108-14.local>
2024-09-02 09:41:41 -0600

    No le se al git

---
+++ Calculadora_Lola/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Calculadora_Lola
+//
+//  Created by alumno on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var boton_para_interactuar: UIButton!
+    @IBOutlet weak var texto_a_cambiar: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func que_hacer_al_pushar_boton(_ sender: Any) {
+        texto_a_cambiar.text = "Bienvenidos"
+    }
+    
+}
+
